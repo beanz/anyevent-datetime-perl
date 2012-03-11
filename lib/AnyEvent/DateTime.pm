f79bd7708a21dfcd7ca3b74ac777be18e2d9197b
Mark Hindess <soft-github@temporalanomaly.com>
2012-03-11 22:43:52 +0000

    Initial attempt.

--- lib/AnyEvent/DateTime.pm
+++ lib/AnyEvent/DateTime.pm
@@ -2,12 +2,95 @@ use strict;
 use warnings;
 package AnyEvent::DateTime;
 
-# ABSTRACT: Blah blah
+# ABSTRACT: Perl module to create AnyEvent timers from DateTime Events
 
 =head1 SYNOPSIS
 
+  use AnyEvent::DateTime;
+  my $t = AnyEvent::DateTime->new(type => 'Recurrence',
+                                  method => 'hourly',
+                                  arguments => [ minutes => [0,30] ],
+                                  callback => sub { warn "chime\n" });
+  print 'Next chime will be at ', $t->next, "\n";
+
+  use DateTime::Set;
+  my $set = DateTime::Set->from_datetimes(dates => [
+              DateTime->now->truncate(to => 'minute')->add(minutes => 1),
+              DateTime->now->truncate(to => 'minute')->add(minutes => 2),
+              DateTime->now->truncate(to => 'minute')->add(minutes => 3),
+            ]);
+  my $t2;
+  $t2 = AnyEvent::DateTime->new(set => $set,
+                                callback => sub {
+                                  warn 'Next tick at ', $t2->next, "\n"
+                                });
+  print 'Next tick will be at ', $t2->next, "\n";
+  AnyEvent->condvar->recv;
+
 =head1 DESCRIPTION
 
+This module is a wrapper to make it simpler to create various
+recurrent timers with AnyEvent.  It can be used to create timers from
+L<DateTime::Set> objects or from C<DateTime::Event> object.
+
 =cut
+
+use Class::Load qw/try_load_class/;
+use AnyEvent;
+use Scalar::Util qw/weaken/;
+use Carp qw/croak/;
+
+sub new {
+  my $pkg = shift;
+  my $self = bless { }, $pkg;
+
+  my %p = @_;
+  $self->{_callback} = $p{callback};
+  if (exists $p{set}) {
+    $self->{_set} = $p{set};
+  } else {
+    my $type = $p{type};
+    my $method = $p{method} || 'new';
+    my @args = @{$p{arguments} || []};
+    my $module = 'DateTime::Event::'.$type;
+    my ($res, $error) = try_load_class($module);
+    unless ($res) { croak "Invalid timer, $module not available: $error\n"; }
+    $self->{_set} = $module->$method(@args);
+  }
+  $self->_set();
+  $self
+}
+
+sub DESTROY {
+  shift->cancel
+}
+
+sub cancel {
+  my $self = shift;
+  delete $self->{_watcher};
+}
+
+sub next {
+  shift->{_next}
+}
+
+sub _set {
+  my ($self) = shift;
+  my $now = AnyEvent->now;
+  my $next = $self->{_next} =
+    $self->{_set}->next(DateTime->from_epoch(epoch => $now));
+  return unless (defined $next);
+  my $sleep = $next->epoch - $now;
+  weaken $self;
+  $self->{_watcher} =
+    AnyEvent->timer(after => $sleep,
+                    cb => sub {
+                      delete $self->{_watcher};
+                      my $res = $self->{_callback}->();
+                      if ($res) {
+                        $self->_set;
+                      }
+                    });
+}
 
 1;
---
+++ t/01-simple.t
@@ -0,0 +1,57 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Test::More;
+use AnyEvent;
+use Class::Load qw/try_load_class/;
+
+use_ok('AnyEvent::DateTime');
+
+SKIP: {
+  skip 'DateTime::Event::Recurrence not available', 7
+    unless (try_load_class('DateTime::Event::Recurrence'));
+
+  my $count = 2;
+  my $cv = AnyEvent->condvar;
+  my $w = AnyEvent->timer(after => 5, cb => sub { $cv->send(-1) });
+  my $t = AnyEvent::DateTime->new(type => 'Recurrence',
+                                  method => 'secondly',
+                                  arguments => [ interval => 1 ],
+                                  callback => sub {
+                                    my $res = $count;
+                                    $count--;
+                                    $cv->send($res);
+                                    return $res;
+                                  });
+  ok($t, 'timer created');
+  my $now = AnyEvent->now;
+  my $next = $t->next->epoch;
+  ok($next > $now, q!... 'next' later than now!);
+  ok($next < $now+1, q!... 'next' less than now+1!);
+  is($cv->recv, 2, '... timer count=2');
+  $cv = AnyEvent->condvar;
+  is($cv->recv, 1, '... timer count=1');
+  $cv = AnyEvent->condvar;
+  is($cv->recv, 0, '... timer count=0');
+  $cv = AnyEvent->condvar;
+  is($cv->recv, -1, '... timer removed');
+
+  $cv = AnyEvent->condvar;
+  $w = AnyEvent->timer(after => 2, cb => sub { $cv->send(-1) });
+  $t = AnyEvent::DateTime->new(type => 'Recurrence',
+                               method => 'secondly',
+                               arguments => [ interval => 1 ],
+                               callback => sub {
+                                 $cv->send(1);
+                                    1;
+                               });
+  ok($t, 'timer created');
+  undef $t;
+  is($cv->recv, -1, '... timer cancelled');
+};
+
+eval { AnyEvent::DateTime->new(type => 'Lunch'); };
+like($@, qr!^Invalid timer, DateTime::Event::Lunch not available:!,
+     'invalid event module');
+
+done_testing;
