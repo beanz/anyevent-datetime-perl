d77a4afd92292f1e4618e17fb5ecd53b75f0eeba
Mark Hindess <soft-github@temporalanomaly.com>
2014-07-19 14:33:57 +0100

    Build results of 15fead8 (on master)

--- lib/AnyEvent/DateTime.pm
+++ lib/AnyEvent/DateTime.pm
@@ -1,32 +1,33 @@
 use strict;
 use warnings;
 package AnyEvent::DateTime;
-BEGIN {
-  $AnyEvent::DateTime::VERSION = '1.120710';
-}
-
+$AnyEvent::DateTime::VERSION = '1.142000';
 # ABSTRACT: Perl module to create AnyEvent timers from DateTime Events
+
 
 use Class::Load qw/try_load_class/;
 use AnyEvent;
 use Scalar::Util qw/weaken/;
-use Carp qw/croak carp/;
+use Carp qw/croak/;
 
 sub new {
   my $pkg = shift;
-  my $self = bless { }, $pkg;
+  my %p; @p{qw/set tick_cb empty_cb/} = @_;
+  my $self = bless \%p, $pkg;
+  $self->_set();
+  $self
+}
 
-  my (%p) = @_;
-  $self->{_callback} = $p{callback};
+sub new_from_events {
+  my $pkg = shift;
+  my %p = @_;
   my $type = $p{type};
-  my $method = $p{method}||'new';
-  my @args = @{$p{arguments}||[]};
+  my $method = $p{method} || 'new';
+  my @args = @{$p{arguments} || []};
   my $module = 'DateTime::Event::'.$type;
   my ($res, $error) = try_load_class($module);
   unless ($res) { croak "Invalid timer, $module not available: $error\n"; }
-  $self->{_set} = $module->$method(@args);
-  $self->_set();
-  $self
+  $pkg->new($module->$method(@args), $p{callback}, $p{empty_callback});
 }
 
 sub DESTROY {
@@ -46,14 +47,18 @@ sub _set {
   my ($self) = shift;
   my $now = AnyEvent->now;
   my $next = $self->{_next} =
-    $self->{_set}->next(DateTime->from_epoch(epoch => $now));
-  my $sleep = $self->{_next}->epoch - $now;
+    $self->{set}->next(DateTime->from_epoch(epoch => $now));
+  unless (defined $next) {
+    $self->{empty_cb}->() if (defined $self->{empty_cb});
+    return;
+  }
+  my $sleep = $next->epoch - $now;
   weaken $self;
   $self->{_watcher} =
     AnyEvent->timer(after => $sleep,
                     cb => sub {
                       delete $self->{_watcher};
-                      my $res = $self->{_callback}->();
+                      my $res = $self->{tick_cb}->();
                       if ($res) {
                         $self->_set;
                       }
@@ -63,7 +68,10 @@ sub _set {
 1;
 
 __END__
+
 =pod
+
+=encoding UTF-8
 
 =head1 NAME
 
@@ -71,7 +79,37 @@ AnyEvent::DateTime - Perl module to create AnyEvent timers from DateTime Events
 
 =head1 VERSION
 
-version 1.120710
+version 1.142000
+
+=head1 SYNOPSIS
+
+  use AnyEvent::DateTime;
+  use DateTime::Set;
+  my $set = DateTime::Set->from_datetimes(dates => [
+              DateTime->now->truncate(to => 'minute')->add(minutes => 1),
+              DateTime->now->truncate(to => 'minute')->add(minutes => 2),
+              DateTime->now->truncate(to => 'minute')->add(minutes => 3),
+            ]);
+  my $t;
+  $t = AnyEvent::DateTime->new(set => $set,
+                               callback => sub {
+                                 warn 'Next tick at ', $t->next, "\n"
+                               });
+  print 'Next tick will be at ', $t->next, "\n";
+
+  my $t2 =
+    AnyEvent::DateTime->new_from_events(type => 'Recurrence',
+                                        method => 'hourly',
+                                        arguments => [ minutes => [0,30] ],
+                                        callback => sub { warn "chime\n" });
+  print 'Next chime will be at ', $t2->next, "\n";
+  AnyEvent->condvar->recv;
+
+=head1 DESCRIPTION
+
+This module is a wrapper to make it simpler to create various
+recurrent timers with AnyEvent.  It can be used to create timers from
+L<DateTime::Set> objects or from C<DateTime::Event> object.
 
 =head1 AUTHOR
 
@@ -79,10 +117,9 @@ Mark Hindess <soft-cpan@temporalanomaly.com>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2012 by Mark Hindess.
+This software is copyright (c) 2014 by Mark Hindess.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
 
 =cut
-
--- t/01-simple.t
+++ t/01-simple.t
@@ -14,15 +14,15 @@ SKIP: {
   my $count = 2;
   my $cv = AnyEvent->condvar;
   my $w = AnyEvent->timer(after => 5, cb => sub { $cv->send(-1) });
-  my $t = AnyEvent::DateTime->new(type => 'Recurrence',
-                                  method => 'secondly',
-                                  arguments => [ interval => 1 ],
-                                  callback => sub {
-                                    my $res = $count;
-                                    $count--;
-                                    $cv->send($res);
-                                    return $res;
-                                  });
+  my $t = AnyEvent::DateTime->new_from_events(type => 'Recurrence',
+                                              method => 'secondly',
+                                              arguments => [ interval => 1 ],
+                                              callback => sub {
+                                                my $res = $count;
+                                                $count--;
+                                                $cv->send($res);
+                                                return $res;
+                                              });
   ok($t, 'timer created');
   my $now = AnyEvent->now;
   my $next = $t->next->epoch;
@@ -38,19 +38,19 @@ SKIP: {
 
   $cv = AnyEvent->condvar;
   $w = AnyEvent->timer(after => 2, cb => sub { $cv->send(-1) });
-  $t = AnyEvent::DateTime->new(type => 'Recurrence',
-                               method => 'secondly',
-                               arguments => [ interval => 1 ],
-                               callback => sub {
-                                 $cv->send(1);
-                                    1;
-                               });
+  $t = AnyEvent::DateTime->new_from_events(type => 'Recurrence',
+                                           method => 'secondly',
+                                           arguments => [ interval => 1 ],
+                                           callback => sub {
+                                             $cv->send(1);
+                                             1;
+                                           });
   ok($t, 'timer created');
   undef $t;
   is($cv->recv, -1, '... timer cancelled');
 };
 
-eval { AnyEvent::DateTime->new(type => 'Lunch'); };
+eval { AnyEvent::DateTime->new_from_events(type => 'Lunch'); };
 like($@, qr!^Invalid timer, DateTime::Event::Lunch not available:!,
      'invalid event module');
 
