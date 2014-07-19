347d213449b1d5fb3a65c02887a33a7b6ab6d1ff
Mark Hindess <soft-github@temporalanomaly.com>
2014-07-19 22:25:30 +0100

    Build results of 15fead8 (on master)

--- lib/AnyEvent/DateTime.pm
+++ lib/AnyEvent/DateTime.pm
@@ -10,6 +10,7 @@ use AnyEvent;
 use Scalar::Util qw/weaken/;
 use Carp qw/croak/;
 
+
 sub new {
   my $pkg = shift;
   my %p; @p{qw/set tick_cb empty_cb/} = @_;
@@ -18,26 +19,30 @@ sub new {
   $self
 }
 
+
 sub new_from_events {
   my $pkg = shift;
   my %p = @_;
   my $type = $p{type};
   my $method = $p{method} || 'new';
   my @args = @{$p{arguments} || []};
-  my $module = 'DateTime::Event::'.$type;
+  my $module = $type =~ /^DateTime::/ ? $type : 'DateTime::Event::'.$type;
   my ($res, $error) = try_load_class($module);
   unless ($res) { croak "Invalid timer, $module not available: $error\n"; }
-  $pkg->new($module->$method(@args), $p{callback}, $p{empty_callback});
+  my $set = $module->$method(@args);
+  $pkg->new($set, $p{callback}, $p{empty_callback});
 }
 
 sub DESTROY {
   shift->cancel
 }
 
+
 sub cancel {
   my $self = shift;
   delete $self->{_watcher};
 }
+
 
 sub next {
   shift->{_next}
@@ -91,8 +96,8 @@ version 1.142000
               DateTime->now->truncate(to => 'minute')->add(minutes => 3),
             ]);
   my $t;
-  $t = AnyEvent::DateTime->new(set => $set,
-                               callback => sub {
+  $t = AnyEvent::DateTime->new($set,
+                               sub {
                                  warn 'Next tick at ', $t->next, "\n"
                                });
   print 'Next tick will be at ', $t->next, "\n";
@@ -110,6 +115,55 @@ version 1.142000
 This module is a wrapper to make it simpler to create various
 recurrent timers with AnyEvent.  It can be used to create timers from
 L<DateTime::Set> objects or from C<DateTime::Event> object.
+
+=head2 C<new($set, $tick_callback, $empty_callback)>
+
+This constructor creates a wrapper around AnyEvent timers which
+triggers at each time in the L<DateTime::Set>, C<$set>. When
+triggered the tick callback is called. The tick callback
+should return true unless it wants all subsequent timers to be cancelled.
+If the set becomes empty then the optional empty callback is called.
+
+=head2 C<new_from_events(\%parameters)>
+
+This constructor creates a wrapper around AnyEvent timers which
+triggers based on a C<DateTime::Event> module. The parameter hash
+has keys:
+
+=over 4
+
+=item type
+
+   The name of the C<DateTime::Event> module to construct the
+   L<DateTime::Set> from. The C<DateTime::Event::> prefix may be left
+   out.
+
+=item method
+
+  The constructor/method to call on the C<DateTime::Event> module.
+  This parameter is optional and defaults to the constructor, C<new>.
+
+=item arguments
+
+  The arguments to pass to the method. This parameter is optional and
+  defaults to the empty list.
+
+=item callback
+
+  The tick callback as described above.
+
+=item empty_callback
+
+  The optional empty callback as described above.
+
+=head2 C<cancel()>
+
+This method cancels any outstanding timer.
+
+=head2 C<cancel()>
+
+This method returns the L<DateTime> object of the next time a timer is
+due to trigger.
 
 =head1 AUTHOR
 
